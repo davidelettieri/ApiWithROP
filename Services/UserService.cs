dbef231739baee5daa126657b5d00461045acadc
davidelettieri <davide.lettieri@gmail.com>
2021-09-29 08:09:54 +0200

    localization and user service update

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ApiWithROP.Models;
 using ROP;
 
@@ -5,18 +6,23 @@ namespace ApiWithROP.Services
 {
     public class UserService
     {
+        private readonly Dictionary<int, User> _source = new()
+        {
+            { 1, new User() { Id = 1, Name = "User 1" } },
+            { 2, new User() { Id = 1, Name = "User 2" } },
+            { 3, new User() { Id = 1, Name = "User 3" } },
+            { 4, new User() { Id = 1, Name = "User 4" } },
+            { 5, new User() { Id = 1, Name = "User 5" } }
+        };
+
         public Result<User> GetUser(int id)
         {
-            if (id < 0)
+            if (!_source.TryGetValue(id, out var user))
             {
                 return Errors.UserNotFound;
             }
 
-            return new User()
-            {
-                Id = id,
-                Name = "Fake user"
-            };
+            return user;
         }
 
     }
--- Utils/ResultsExtensions.cs
+++ Utils/ResultsExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using ROP;
 
 namespace ApiWithROP.Utils
@@ -8,8 +9,16 @@ namespace ApiWithROP.Utils
         public static ActionResult<T> ToActionResult<T>(this Result<T> result)
          => result.Match(FromSuccess, FromFailure<T>);
 
+        public static ActionResult<T> ToActionResult<T>(this Result<T> result, IStringLocalizer localizer)
+         => result.Match(FromSuccess, f => FromFailure<T>(f, localizer));
+
 
         private static ActionResult<T> FromSuccess<T>(T t) => new OkObjectResult(t);
         private static ActionResult<T> FromFailure<T>(Failure f) => new ObjectResult(f) { StatusCode = 403 };
+        private static ActionResult<T> FromFailure<T>(Failure f, IStringLocalizer localizer)
+        {
+            var localizedFailure = new Failure(f.ReasonId, localizer[f.ReasonId]);
+            return new ObjectResult(localizedFailure) { StatusCode = 403 };
+        }
     }
 }
